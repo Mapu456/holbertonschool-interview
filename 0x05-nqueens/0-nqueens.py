2e683ce7ae2188bfcb627c31ab93adb8b218eb8c
Mapu456 <mpaula456@hotmail.com>
2022-05-22 16:20:15 -0500

    adding proyect

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,34 +1,55 @@
 #!/usr/bin/python3
+"""Solving N Queens with Backtracing"""
 import sys
 
 
-def queens(n, i, a, b, c):
-    '''
-    Calculates every possible solution to the n queen puzzle
-    '''
-    if i < n:
-        for j in range(n):
-            if j not in a and i+j not in b and i-j not in c:
-                yield from queens(n, i+1, a+[j], b+[i+j], c+[i-j])
-    else:
-        yield a
+def nqueens(n, y, board):
+    """
+    Method: nqueens - place n queens
+            on an n by n board so that
+            no queens are attacking any
+            others.
+    Parameters: n is an int that sets
+                board size and # of queens
+    Return: All possible solutions to
+            placement, in list of lists form
+    """
+    for x in range(n):
+        hold = 0
+        for q in board:
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
 
 
-if sys.argv.__len__() != 2:
-    print('Usage: nqueens N ')
-    sys.exit(1)
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-n = int(sys.argv[1])
-if not isinstance(n, int):
-    print('N must be a number')
-    sys.exit(1)
+    nqueens(n, 0, [])
 
-if n < 4:
-    print('N must be at least 4')
-    sys.exit(1)
 
-for y_coordinates in queens(n, 0, [], [], []):
-    x_coordinates = [[x] for x in range(0, n)]
-    for i, x in enumerate(x_coordinates):
-        x.append(y_coordinates[i])
-    print(x_coordinates)
+if __name__ == '__main__':
+    main()
